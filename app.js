5cb71c6663100eee97f5d6aaf7e67374e497c0eb
arbadsha00 <arbadsha00@gmail.com>
2024-09-20 19:25:13 +0600

    grand price section done

--- app.js
+++ app.js
@@ -1,3 +1,62 @@
+let seatArr = [];
+const seatCount = document.getElementById("seatCount");
+const totalSeat = document.getElementById("totalSeat");
+const seatLists = document.getElementById("seatLists");
+const totalPrice = document.getElementById("totalPrice");
+const couponField = document.getElementById("couponText");
+const discountBtn = document.getElementById("discountBtn");
+const totalDiscount = document.getElementById("totalDiscount");
+let totalPrices = parseInt(totalPrice.innerText);
+let totalSeats = parseInt(totalSeat.innerText);
+const grandTotal = document.getElementById("grandPrice");
+const discount = document.getElementById("discount");
+let discounts = parseInt(discount.innerText);
+
 function seatSelected(event) {
-    console.log(event.innerText);
+  if (seatArr.includes(event.innerText)) {
+    return alert("One Person Per Seat");
+  }
+  seatArr.push(event.innerText);
+
+  if (seatArr.length > 4) {
+    return alert("Maximum 4");
+  }
+  event.classList.add("bg-main", "text-white");
+  console.log(seatArr);
+  seatCount.innerText = seatArr.length;
+  totalSeats--;
+  totalSeat.innerText = totalSeats;
+  seatLists.innerHTML += `<li class="flex justify-between text-xl text-gray-500">
+                    <span>${event.innerText}</span> <span>Economy</span> <span>550</span>
+                  </li>`;
+  totalPrices += 550;
+  totalPrice.innerText = totalPrices;
+  if (seatArr.length > 3) {
+    couponField.removeAttribute("disabled");
+    discountBtn.removeAttribute("disabled");
+    totalDiscount.classList.remove("hidden");
+    grandTotal.innerText = totalPrices;
+  }
 }
+
+document.getElementById("discountBtn").addEventListener("click", function () {
+  if (couponField.value == "") {
+    return alert("Enter Coupon Code");
+  } else if (couponField.value == "NEW15" && discounts == 0) {
+    discounts = totalPrices * 0.15;
+    discount.innerText = discounts;
+    totalPrices -= discounts;
+    grandTotal.innerText = totalPrices;
+    return;
+  } else if (couponField.value == "COUPLE20" && discounts == 0) {
+    discounts = totalPrices * 0.2;
+    discount.innerText = discounts;
+    totalPrices -= discounts;
+    grandTotal.innerText = totalPrices;
+    return;
+  }
+});
+
+document.getElementById("buy").addEventListener("click", function () {
+    document.getElementById("buy-section").scrollIntoView({behavior:"smooth"})
+})
